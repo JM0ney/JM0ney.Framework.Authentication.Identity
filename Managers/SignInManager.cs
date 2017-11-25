2c26b79fde00726b1514afb9db91284667e6e771
Jason-Iverson.com <contact@jason-iverson.com>
2017-11-25 14:42:32 -0800

    Oops

--- Managers/SignInManager.cs
+++ Managers/SignInManager.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.Web;
 
 namespace JM0ney.Framework.Authentication.Identity.Managers {
-    class SignInManager {
+
+    public class SignInManager : SignInManager<UserManager, User, UserLogin, UserRole, UserClaim> {
+
+        public SignInManager( UserManager userManager, IAuthenticationManager authenticationManager )
+          : base( userManager, authenticationManager ) { }
+
+        public static SignInManager Create( IdentityFactoryOptions<SignInManager> options, IOwinContext context ) {
+            return new SignInManager( context.GetUserManager<UserManager>( ), context.Authentication );
+        }
     }
+
+
+    public class SignInManager<TUserManager, TUser, TUserLogin, TUserRole, TUserClaim> : SignInManager<TUser, Guid>
+        where TUserManager : UserManager<TUser, TUserLogin, TUserRole, TUserClaim>
+        where TUser : UserBase<TUser, TUserLogin, TUserRole, TUserClaim> 
+        where TUserLogin : IdentityUserLogin<Guid>
+        where TUserRole : IdentityUserRole<Guid>
+        where TUserClaim : IdentityUserClaim<Guid> {
+
+        public SignInManager( TUserManager userManager, IAuthenticationManager authenticationManager )
+            : base( userManager, authenticationManager ) { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync( TUser user ) {
+            // Going to need to troubleshoot this
+            TUserManager userManager = ( TUserManager ) this.UserManager;
+            return user.GenerateUserIdentityAsync( userManager );
+        }
+    }
+
 }
